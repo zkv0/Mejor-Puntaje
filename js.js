5dd740b0efba0e1bf685d3e41990d400a88687fb
ah4280mx <rdroxhk5@duck.com>
2023-05-21 22:44:07 -0500

    correcion de error en js al guardar datos

--- js.js
+++ js.js
@@ -2,7 +2,39 @@ var boton = document.getElementById("boton");
 var nav = document.getElementById("nav");
 var body = document.getElementById("body");
 
+
+
+
+
+
+
+// Verificar si hay un estado guardado en el almacenamiento local
+var botonPresionado = localStorage.getItem("botonPresionado");
+if (botonPresionado === "true") {
+  boton.classList.add("pressed");
+}
+
+
+
 boton.addEventListener("click", function() {
   nav.classList.toggle("dark");
   body.classList.toggle("dark");
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (boton.classList.contains("pressed")) {
+    localStorage.setItem("botonPresionado", "true");
+  } else {
+    localStorage.removeItem("botonPresionado");
+  };
