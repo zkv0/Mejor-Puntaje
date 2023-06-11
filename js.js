2c89e36891871927b705ec60ecb7359c80bc947f
ah4280mx <rdroxhk5@duck.com>
2023-06-10 19:07:52 -0500

    pagina principal terminada

---
+++ js.js
@@ -0,0 +1,18 @@
+var lastScrollTop = 0;
+var supernavmega = document.getElementById("supernavmega");
+
+window.addEventListener("scroll", function() {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    // Desplazamiento hacia abajo: Ocultar la barra de navegación
+    supernavmega.style.transform = "translateY(-100%)";
+    supernavmega.style.transition = "transform 0.3s ease-out";
+  } else {
+    // Desplazamiento hacia arriba: Mostrar la barra de navegación
+    supernavmega.style.transform = "translateY(0)";
+    supernavmega.style.transition = "transform 0.3s ease-out";
+  }
+
+  lastScrollTop = scrollTop;
+});
