150d2a520b17dcbb2a489f8c8578e6c52fb1d991
ah4280mx <rdroxhk5@duck.com>
2023-06-10 22:17:39 -0500

    menu abajo en celulares

--- js.js
+++ js.js
@@ -3,19 +3,33 @@ var supernavmega = document.getElementById("supernavmega");
 
 window.addEventListener("scroll", function() {
   var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  var windowWidth = window.innerWidth;
 
-  if (scrollTop > lastScrollTop) {
-    // Desplazamiento hacia abajo: Ocultar la barra de navegación
-    supernavmega.style.transform = "translateY(-100%)";
-    supernavmega.style.transition = "transform 0.3s ease-out";
+  if (windowWidth > 600) {
+    if (scrollTop > lastScrollTop) {
+      // Desplazamiento hacia abajo: Ocultar la barra de navegación
+      supernavmega.style.transform = "translateY(-100%)";
+      supernavmega.style.transition = "transform 0.3s ease-out";
+    } else {
+      // Desplazamiento hacia arriba: Mostrar la barra de navegación
+      supernavmega.style.transform = "translateY(0)";
+      supernavmega.style.transition = "transform 0.3s ease-out";
+    }
   } else {
-    // Desplazamiento hacia arriba: Mostrar la barra de navegación
-    supernavmega.style.transform = "translateY(0)";
-    supernavmega.style.transition = "transform 0.3s ease-out";
+    if (scrollTop > lastScrollTop) {
+      // Desplazamiento hacia abajo: Ocultar la barra de navegación
+      supernavmega.style.transform = "translateY(100%)";
+      supernavmega.style.transition = "transform 0.3s ease-out";
+    } else {
+      // Desplazamiento hacia arriba: Mostrar la barra de navegación
+      supernavmega.style.transform = "translateY(0)";
+      supernavmega.style.transition = "transform 0.3s ease-out";
+    }
   }
 
   lastScrollTop = scrollTop;
 });
+
 
 
 
